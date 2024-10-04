585b397232051e917acda7ad603aabe208d60a31
abodesigner <mohammed.abosalim@gmail.com>
2024-10-04 07:30:18 +0300

    adding frontend project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-center">
+                LMS - FROM SCRATCH - USING REACT.JS- TS
+            </h1>
+
+            <Button>Click me</Button>
+        </>
+    );
+}
+
+export default App;
